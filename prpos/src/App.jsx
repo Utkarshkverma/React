64655c0b535180af8621fe6eff1d7c17bb2aea90
Utkarsh <vermau2k01@gmail.com>
2024-02-29 11:56:10 +0530

    Props in React

---
+++ prpos/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Card from './Card'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Card text = "Hello"/>
+      <Card text = "lorem5"/>
+    </>
+  )
+}
+
+export default App
